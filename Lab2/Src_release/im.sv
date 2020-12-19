94e046aac2ab12fdd5b921ee595900f6d560b6a4
PangTiger <1346658359@qq.com>
2020-12-19 16:11:58 +0800

    Fix bugs in im.sv

--- Lab2/Src_release/im.sv
+++ Lab2/Src_release/im.sv
@@ -78,15 +78,15 @@ module im (
       */
       
       //Data dependence test case begin
-      
-      mem[0] = inst_t'({32'd0})                                       ;
-      mem[1] = inst_t'({ LUI, 5'h0, REG_AT, 16'h1234})                ; 
-      mem[2] = inst_t'({ ORI, REG_AT, REG_AT, 16'habcd})              ;
-      mem[3] = inst_t'({ LUI, 5'h0, REG_V0, 16'h1230})                ;
-      mem[4] = inst_t'({ ORI, REG_V0, REG_V0, 16'habcd})              ;
-      mem[5] = inst_t'({ 6'h0, REG_AT, REG_V0, REG_V1, 5'h0, SUBU})   ;
-      mem[6] = inst_t'({ 6'h0, REG_V0, REG_V1, REG_ZERO, 5'h0, MULT}) ;
-      mem[7] = inst_t'({32'd0})                                       ;
+//      
+//      mem[0] = inst_t'({32'd0})                                       ;
+//      mem[1] = inst_t'({ LUI, 5'h0, REG_AT, 16'h1234})                ; 
+//      mem[2] = inst_t'({ ORI, REG_AT, REG_AT, 16'habcd})              ;
+//      mem[3] = inst_t'({ LUI, 5'h0, REG_V0, 16'h1230})                ;
+//      mem[4] = inst_t'({ ORI, REG_V0, REG_V0, 16'habcd})              ;
+//      mem[5] = inst_t'({ 6'h0, REG_AT, REG_V0, REG_V1, 5'h0, SUBU})   ;
+//      mem[6] = inst_t'({ 6'h0, REG_V0, REG_V1, REG_ZERO, 5'h0, MULT}) ;
+//      mem[7] = inst_t'({32'd0})                                       ;
       //mem[7] = inst_t'({ 16'h0, REG_A0,5'h0,MFHI})                    ;
       //mem[8] = inst_t'({ 16'h0, REG_A1,5'h0,MFLO})                    ;
       //mem[9] = inst_t'({32'd0})                                       ; 
@@ -94,47 +94,46 @@ module im (
       //Data dependence test case end
       
       //Simple jump inst test case begin
-      /*                                
-      mem[1] = inst_t'({ LUI, 5'h0, REG_AT, 16'h1})                   ;
-      mem[2] = inst_t'({ J, 26'h14})                                  ;
-      mem[3] = inst_t'({32'd0})                                       ;
-      mem[4] = inst_t'({ JAL, 26'h20})                                ;
-      mem[5] = inst_t'({ LUI, 5'h0, REG_AT, 16'h3})                   ;
-      mem[6] = inst_t'({ ADDI, REG_ZERO, REG_V0, 16'hc})              ;
-      mem[7] = inst_t'({ 6'h0, REG_V0, REG_ZERO, REG_ZERO, 5'h0, JR}) ;
-      mem[8] = inst_t'({ LUI, 5'h0, REG_AT, 16'h2})                   ;
-      mem[9] = inst_t'({ BNE, REG_V0, REG_AT, 16'h2})                 ;
-      mem[10]= inst_t'({ LUI, 5'h0, REG_AT, 16'h4})                   ;
-      mem[11]= inst_t'({ LUI, 5'h0, REG_AT, 16'hA})                   ;
-      mem[12]= inst_t'({ LUI, 5'h0, REG_AT, 16'h5})                   ;
-      mem[13]= inst_t'({ LUI, 5'h0, REG_AT, 16'h6})                   ;
-      mem[14]= inst_t'({ BEQ, REG_V0, REG_AT, 16'hfffc})              ;
-      mem[15]= inst_t'({32'd0})                                       ;
-      mem[16]= inst_t'({ J, 26'h3c})                                  ;
-      mem[17]= inst_t'({32'd0})                                       ;
-      */
+                              
+//      mem[0] = inst_t'({ LUI, 5'h0, REG_AT, 16'h1})                   ;
+//      mem[1] = inst_t'({ J, 26'h14})                                  ;
+//      mem[2] = inst_t'({32'd0})                                       ;
+//      mem[3] = inst_t'({ JAL, 26'h20})                                ;
+//      mem[4] = inst_t'({ LUI, 5'h0, REG_AT, 16'h3})                   ;
+//      mem[5] = inst_t'({ ADDI, REG_ZERO, REG_V0, 16'hc})              ;
+//      mem[6] = inst_t'({ 6'h0, REG_V0, REG_ZERO, REG_ZERO, 5'h0, JR}) ;
+//      mem[7] = inst_t'({ LUI, 5'h0, REG_AT, 16'h2})                   ;
+//      mem[8] = inst_t'({ BNE, REG_V0, REG_AT, 16'h2})                 ;
+//      mem[9]  = inst_t'({ LUI, 5'h0, REG_AT, 16'h4})                   ;
+//      mem[10]= inst_t'({ LUI, 5'h0, REG_AT, 16'hA})                   ;
+//      mem[11]= inst_t'({ LUI, 5'h0, REG_AT, 16'h5})                   ;
+//      mem[12]= inst_t'({ LUI, 5'h0, REG_AT, 16'h6})                   ;
+//      mem[13]= inst_t'({ BEQ, REG_V0, REG_AT, 16'hfffc})              ;
+//      mem[14]= inst_t'({32'd0})                                       ;
+//      mem[15]= inst_t'({ J, 26'h3c})                                  ;
+//      mem[16]= inst_t'({32'd0})                                       ;
+      
       //Simple jump inst test case end
       
       //load dependence test case begin
       /*
-      mem[1] = inst_t'({ ORI, REG_ZERO, REG_AT, 16'habab})            ;
-      mem[2] = inst_t'({ SW, REG_ZERO, REG_AT, 16'h1000})             ;
-      mem[3] = inst_t'({ ORI, REG_ZERO, REG_V0, 16'habab})            ;
-      mem[4] = inst_t'({ ORI, REG_ZERO, REG_AT, 16'h0})               ;
-      mem[5] = inst_t'({ LW, REG_ZERO, REG_V1, 16'h1000})             ;
-      mem[6] = inst_t'({ BEQ, REG_V1, REG_V0, 16'h4})                 ;
-      mem[7] = inst_t'({ 32'd0})                                      ;
-      mem[8] = inst_t'({ ORI, REG_ZERO, REG_AT, 16'h1234})            ;
-      mem[9] = inst_t'({ 32'd0})                                      ;
-      mem[10]= inst_t'({ J, 26'h30})                                  ;
-      mem[11]= inst_t'({ ORI, REG_ZERO, REG_AT, 16'hfefe})            ;
-      mem[12]= inst_t'({ 32'd0})                                      ;
-      mem[13]= inst_t'({ ORI, REG_ZERO, REG_A0, 16'hffff})            ;
-      mem[14]= inst_t'({ J, 26'h34})                                  ;
-      mem[15]= inst_t'({ 32'd0})                                      ;
+      mem[0] = inst_t'({ ORI, REG_ZERO, REG_AT, 16'habab})            ;
+      mem[1] = inst_t'({ SW, REG_ZERO, REG_AT, 16'h1000})             ;
+      mem[2] = inst_t'({ ORI, REG_ZERO, REG_V0, 16'habab})            ;
+      mem[3] = inst_t'({ ORI, REG_ZERO, REG_AT, 16'h0})               ;
+      mem[4] = inst_t'({ LW, REG_ZERO, REG_V1, 16'h1000})             ;
+      mem[5] = inst_t'({ BEQ, REG_V1, REG_V0, 16'h4})                 ;
+      mem[6] = inst_t'({ 32'd0})                                      ;
+      mem[7] = inst_t'({ ORI, REG_ZERO, REG_AT, 16'h1234})            ;
+      mem[8] = inst_t'({ 32'd0})                                      ;
+      mem[9] = inst_t'({ J, 26'h30})                                  ;
+      mem[10]= inst_t'({ ORI, REG_ZERO, REG_AT, 16'hfefe})            ;
+      mem[11]= inst_t'({ 32'd0})                                      ;
+      mem[12]= inst_t'({ ORI, REG_ZERO, REG_A0, 16'hffff})            ;
+      mem[13]= inst_t'({ J, 26'h34})                                  ;
+      mem[14]= inst_t'({ 32'd0})                                      ;
+      //dependence test case end
       */
-      //load dependence test case end
-      
       
     end
 
